bb00a83ffb75dd7d11a8e03adf9f4f3e72d11e5d
slewandowski <st.lewy@gmail.com>
2022-04-29 21:47:02 +0200

    Initial commit

---
+++ src/main/java/com/sl/cargomail/CargoMailApplication.java
@@ -0,0 +1,13 @@
+package com.sl.cargomail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CargoMailApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CargoMailApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sl/cargomail/CargoMailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sl.cargomail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CargoMailApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
